6fd1b2b7fcabc00d4dc9c0ce1b503c0a5baf859c
Youssof <youssoftamer21@gmail.com>
2023-07-17 16:30:00 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
 /**
  * main - entry point
  * Return: returns 0 always for success
@@ -21,4 +19,6 @@ int main(void)
 	}
 	else {
 		printf("%d is negative\n", n);
-	return (0);}
+	}
+	return (0);
+}
