9fdac51459c84fe7f4c41f284bcc5eefeeaf729d
Youssof <youssoftamer21@gmail.com>
2023-07-18 15:32:20 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - commute the abs vale of no
+ * @n: no to commute on
+ * Return: ALways abs no
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
