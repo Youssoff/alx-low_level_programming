cf22c4d7e43b772ef8371bc0bed0bd3c9f1c0b33
Youssof <youssoftamer21@gmail.com>
2023-07-18 14:58:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ * Description: print alph.
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
