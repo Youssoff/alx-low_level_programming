6f47c09d2f94b0731ed4dea7d03f473d382f9a35
Youssof <youssoftamer21@gmail.com>
2023-07-17 17:28:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - outputs a statement
+ * Description: 'print for 0 to 9'
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
