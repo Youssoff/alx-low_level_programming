2ae87984f5ce16c6be49ec26876dd9abc3473e32
Youssof <youssoftamer21@gmail.com>
2023-07-18 15:59:53 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two ints
+ * @n: is first no to be added
+ * @m: is seconf to be added
+ * Return: the sum
+ */
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
