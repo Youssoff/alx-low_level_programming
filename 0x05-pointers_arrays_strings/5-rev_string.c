1baf6912fbb94a0ac9a568be5913b448ce55f0d3
Youssof <youssoftamer21@gmail.com>
2023-07-25 17:35:15 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	int i;
+	char temp;
+
+	while (s[l])
+		l++;
+
+	for (i = 0; i < l; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
