1913cd353e38514dfee05cf4590e892fe0055634
Youssof <youssoftamer21@gmail.com>
2023-07-25 18:17:00 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 	{
 		if (i != n)
 		{
-			printf("%d, ", a[i]);
+			printf("%d, ", a[i - 1]);
 		}
 		else
 		{
-			printf("%d", a[i]);
+			printf("%d", a[i - 1]);
 		}
 	}
 }
