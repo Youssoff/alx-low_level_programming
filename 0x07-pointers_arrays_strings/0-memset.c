fc9a1ddcc74a12b54bc399039cd2a6e3f25b6474
Youssof <youssoftamer21@gmail.com>
2023-07-31 13:46:25 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - set some chars to a memory location
+ * @s: memory of chars to stored in
+ * @b: char to fill by
+ * @n: no. of chars to be filled
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
