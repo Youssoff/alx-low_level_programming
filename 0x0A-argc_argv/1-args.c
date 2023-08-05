f55d7c212cfb4931f7c532efb240b35ed111a32b
Youssof <youssoftamer21@gmail.com>
2023-08-04 17:39:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints no of args
+ * @argc: no of args
+ * @argv: pointers to an array of strings for the values of arg
+ * Return: always 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc - 1;
+	return (0);
+}
