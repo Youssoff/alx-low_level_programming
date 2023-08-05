197561322bac52bea868bfc4734e0ae05430385d
Youssof <youssoftamer21@gmail.com>
2023-08-04 17:40:36 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: pointers to an array of strings for the values of arg
  * Return: always 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%d", argc - 1;
 	return (0);
