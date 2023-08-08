04ed280ed50ac8be73eaf31ff30d30e326cdca16
Youssof <youssoftamer21@gmail.com>
2023-08-08 10:08:29 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return ('\0');
-
 	char *str;
 	unsigned int i;
+	
+	if (size == 0)
+		return ('\0');
 
 	i = 0;
 	str = malloc(sizeof(char) * size);
